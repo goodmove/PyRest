369f2f6afc6c890041b1a930df7516bf344ec813
Alexey Boltava <goodmoveab@gmail.com>
2017-11-19 17:31:52 +0700

    Improve http response classes. Add JsonResponse class for convenient json responses

--- pyrest/http.py
+++ pyrest/http.py
@@ -1,3 +1,6 @@
+import json
+
+
 class HttpRequest:
 
     class RequestMethod:
@@ -18,5 +21,44 @@ class HttpRequest:
     def parse_url(self, url: str):
         self.path = url
 
+
 class HttpResponse:
-    pass
+
+    def __init__(self, code: int = 200, message: str = 'OK'):
+        self.__code = code
+        self.__message = message
+        self.__headers = dict()
+        self.__body = ''
+
+    def add_header(self, header_name: str, header_value: str):
+        self.__headers[header_name] = header_value
+
+        return self
+
+    def get_headers(self) -> dict:
+        return self.__headers
+
+    def set_body(self, body: str):
+        self.__body = body
+
+    def get_body(self) -> bytes:
+        return bytes(self.__body, 'utf-8')
+
+    def get_code(self) -> int:
+        return self.__code
+
+    def get_message(self):
+        return self.__message
+
+
+class HttpJsonResponse(HttpResponse):
+
+    def __init__(self, json_obj: dict, code: int = 200, message: str = 'OK'):
+        super(HttpJsonResponse, self).__init__(code, message)
+        self.__json_obj = json_obj
+        self.__json_str = json.dumps(json_obj)
+        self.add_header('Content-Type', 'application/json')
+        self.add_header('Content-Type', 'application/json')
+        self.add_header('Content-Length', str(len(self.__json_str)))
+
+        self.set_body(self.__json_str)
