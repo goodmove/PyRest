3c633ad49ca5d90f50215657e18823e0d89d1ca3
Alexey Boltava <goodmoveab@gmail.com>
2017-11-19 17:32:44 +0700

    Modify server requests handler to respond with the help of HttpResponseClass

--- pyrest/src/pyrest.py
+++ pyrest/src/pyrest.py
@@ -7,8 +7,14 @@ from pyrest.src.router import Router
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         print('new request')
-        Router().resolve(HttpRequest(self.path, HttpRequest.methods.get))
-        self.send_response_only(200, 'OK')
+
+        http_response = Router().resolve(HttpRequest(self.path, HttpRequest.methods.get))
+        self.send_response(http_response.get_code(), http_response.get_message())
+        for name, value in http_response.get_headers().items():
+            self.send_header(name, value)
+        self.end_headers()
+
+        self.wfile.write(http_response.get_body())
 
 
 DEFAULT_SERVER_ADDRESS = ('', 8000)
