523e29eb71aa1c3669467bb00307ab20ec04f5a6
Alexey Boltava <goodmoveab@gmail.com>
2017-11-20 16:26:43 +0700

    Extend request/response support for PyRest server
    Add support for GET, POST, DELETE, and UPDATE http methods.
    Modify response mechanism to send 404 and 500 errors for corresponding python exceptions, which may occur during path resolution.

--- pyrest/src/pyrest.py
+++ pyrest/src/pyrest.py
@@ -1,20 +1,47 @@
 from http.server import BaseHTTPRequestHandler
 
-from pyrest.http import HttpRequest
+import sys
+
+from pyrest.http import HttpRequest, HttpResponse
+from pyrest.src.exceptions import *
 from pyrest.src.router import Router
+
+DEFAULT_SERVER_ADDRESS = ('', 8000)
 
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        print('new request')
+        print('new GET request')
+        self.__handle_request()
 
-        http_response = Router().resolve(HttpRequest(self.path, HttpRequest.methods.get))
+    def do_POST(self):
+        print('new POST request')
+        self.__handle_request()
+
+    def do_UPDATE(self):
+        print('new UPDATE request')
+        self.__handle_request()
+
+    def do_DELETE(self):
+        print('new DELETE request')
+        self.__handle_request()
+
+    def __handle_request(self):
+        try:
+            http_response = Router().resolve(HttpRequest(self.path, HttpRequest.methods.get))
+        except  (NoRouteFoundError,
+                MethodNotDefinedError,
+                InvalidParameterValueError) as error:
+            http_response = HttpResponse(404, 'Not Found. ' + repr(error))
+        except Exception as error:
+            print(repr(error), file=sys.stderr)
+            http_response = HttpResponse(500, 'Internal server error: ' + repr(error))
+        self.__send_response(http_response)
+
+    def __send_response(self, http_response: HttpResponse):
         self.send_response(http_response.get_code(), http_response.get_message())
         for name, value in http_response.get_headers().items():
             self.send_header(name, value)
         self.end_headers()
 
         self.wfile.write(http_response.get_body())
-
-
-DEFAULT_SERVER_ADDRESS = ('', 8000)
