302e27f269546ccc2c8fd2e76f7ae5ed032ed0d2
Alexey Boltava <goodmoveab@gmail.com>
2017-11-23 00:21:28 +0700

    Add constant to map response code to response message

--- pyrest/http.py
+++ pyrest/http.py
@@ -107,3 +107,15 @@ class Headers:
     content_type = 'Content-Type'
     content_length = 'Content-Length'
     auth = 'Authorization'
+
+
+class ResponseMessages:
+    messages = {
+        400: 'Bad Request',
+        401: 'Unauthorized',
+        402: 'Payment Required',
+        403: 'Forbidden',
+        404: 'Not Found',
+        405: 'Method Not Allowed',
+        500: 'Internal Server Error'
+    }
