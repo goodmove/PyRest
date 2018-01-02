a59697cdf1fb492c9e721cb00f79dd96ded1ec22
Alexey Boltava <goodmoveab@gmail.com>
2018-01-02 21:54:21 +0700

    Fix bugs in tree parser

--- pyrest/parser/tree_parser.py
+++ pyrest/parser/tree_parser.py
@@ -1,13 +1,12 @@
 import re
 
-import sys
-
 from pyrest.http import HttpRequest, HttpResponse
 from pyrest.parser.route_parser import AbstractRouteParser
 from pyrest.src.exceptions import *
 from pyrest.src.route import RouteParameters
 
 
+schema_pattern = r"^((\/\w+(-\w+)*)|(\/{\w+(:(int|word|alpha))?}))+$"
 parameter_pattern = r"^{[_a-zA-Z]\w+(:(int|word|alpha))?}$"
 resource_name_pattern = r"^(\w+(-\w+)*)$"
 
@@ -22,6 +21,7 @@ class TreeNode:
         self.route_controller = None
         self.schema = ''
         self.handlers = dict()
+
 
     def get_children(self) -> dict:
         return self.children
@@ -55,14 +55,14 @@ class ParamTreeNode(TreeNode):
             'int': int,
             'alpha': str,
             'word:': str,
-            'any': str,
+            'any:': str,
         }.get('any' if value_type is None else value_type)
 
         self.value_pattern = {
             'int': r"^\d+$",
             'alpha': r"^[a-zA-Z]+$",
             'word': r"^\w+$",
-            'any': r".+",
+            'any': r".+"
         }.get('any' if value_type is None else value_type)
 
     def parse(self, param_string: str):
@@ -106,7 +106,7 @@ class TreeRouteParser(AbstractRouteParser):
                     value = param.parse(url_parts[i])
                     param_values.append(value)
                 except InvalidParameterValueError as e:
-                    print(repr(e), file=sys.stderr)
+                    print(repr(e))
                     is_param_value_invalid = True
                     invalid_param_value_error = e
                 node = param
@@ -130,12 +130,17 @@ class TreeRouteParser(AbstractRouteParser):
         return self.__build_tree_path(schema)
 
     def __build_routes(self, class_routes: dict):
+        print('\n\nbuilding tree\n\n')
         for controller_class, route_params_list in class_routes.items():
             controller_instance = controller_class()
             for route_params in route_params_list:
                 self.__insert_in_tree(controller_instance, route_params)
 
+        self.__print_tree()
+
     def __insert_in_tree(self, controller_instance, params: RouteParameters):
+        print("Inserting schema: " + params.schema)
+
         route_tree_node = self.insert_schema(params.schema)
 
         # if route_tree_node.is_route:
@@ -147,6 +152,7 @@ class TreeRouteParser(AbstractRouteParser):
         route_tree_node.add_handler(params.http_method, params.handler)
 
     def __print_tree(self):
+        print('\n\n**** printing tree ****\n\n')
         nodes_queue = []
 
         if self._routes_tree is not None:
@@ -154,7 +160,7 @@ class TreeRouteParser(AbstractRouteParser):
 
         while len(nodes_queue) > 0:
             node = nodes_queue.pop(0)
-            # print(node.name)
+            print(node.name)
             children = node.get_children()
 
             if children is None:
@@ -168,6 +174,7 @@ class TreeRouteParser(AbstractRouteParser):
                 nodes_queue.append(parameter)
 
     def __build_tree_path(self, schema: str) -> TreeNode:
+        print('schema: ' + schema)
         if len(schema) == 0:
             raise InvalidSchemaFormatError('Schema length must be greater that zero')
 
@@ -186,30 +193,29 @@ class TreeRouteParser(AbstractRouteParser):
 
         for i in range(1, schema_parts_count-1):
             schema_part = str(schema_parts[i])
-            # print('___ parsing ' + schema_part + ' ___')
+            print('___ parsing ' + schema_part + ' ___')
             if len(schema_part) == 0:
                 raise InvalidSchemaFormatError('Repeating slashes are not allowed')
 
             if re.match(resource_name_pattern, schema_part):
-                # print('matched resource name')
+                print('matched resource name')
                 tree_node = current_tree_node.get_child(schema_part)
                 if not tree_node:
                     tree_node = TreeNode(schema_part)
                 current_tree_node.add_child(tree_node)
                 current_tree_node = tree_node
             elif re.match(parameter_pattern, schema_part):
-                # print('matched parameter pattern')
+                print('matched parameter pattern')
                 param_node = current_tree_node.get_parameter()
-                schema_parameter = self.__parse_parametrized_part(schema_part)
-                # print(schema_parameter)
-                if not param_node or param_node.name == schema_parameter[0]:
+                if not param_node:
+                    schema_parameter = self.__parse_parametrized_part(schema_part)
                     if schema_parameter[0] in schema_parameter_names:
                         raise DoubleDeclarationError('Double declaration of parameter ' + str(schema_parameter[0]) +
                                                      ' in schema ' + schema)
                     schema_parameter_names.add(schema_parameter[0])
-                    param_node = ParamTreeNode(schema_parameter[0], schema_parameter[1])
+                    param_node = ParamTreeNode(schema_part, schema_parameter[1])
                 else:
-                    # print(param_node.name)
+                    print(param_node.name)
                     raise RuntimeError('Error for schema ' + schema +
                                        ': any route part can only contain one parametrized child.')
                 current_tree_node.set_parameter(param_node)
@@ -222,11 +228,11 @@ class TreeRouteParser(AbstractRouteParser):
     def __parse_parametrized_part(self, schema_part: str) -> tuple:
         parameter_parts = schema_part.strip('{}').split(':')
         parameter_name = parameter_parts[0].strip()
-        # print('retrieved parameter name:' + parameter_name)
+        print('retrieved parameter name:' + parameter_name)
 
         parameter_type = None
         if len(parameter_parts) > 1:
             parameter_type = parameter_parts[1].strip()
-            # print('retrieved parameter type:' + parameter_name)
+            print('retrieved parameter type:' + parameter_name)
 
-        return parameter_name, parameter_type
+        return (parameter_name, parameter_type)
